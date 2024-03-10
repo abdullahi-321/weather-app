86b7ad0d649b34e9c71856a846ce735d7fddaeb4
Abdul <aladaabdullahi@gmail.com>
2024-03-10 13:35:37 +0100

    added error handling

--- my-react-app/src/component.jsx
+++ my-react-app/src/component.jsx
@@ -7,15 +7,23 @@ function Component(props) {
     let [icon, setIcon] = useState("");
 
     async function getApi() {
-        let raw;
-        const sss = await fetch(`http://api.weatherapi.com/v1/current.json?key=0b0b7c9d31e5469a967173511240503&q=${props.city}}&aqi=no`)
-            .then(data => data.json())
-            .then(data => raw = data)
-
-        console.log(raw);
-        setTemp(raw.current.feelslike_c);
-        setIcon(raw.current.condition.icon);
-        setLocation(`${raw.location.name}`);
+        try {
+            let raw;
+            const sss = await fetch(`http://api.weatherapi.com/v1/current.json?key=0b0b7c9d31e5469a967173511240503&q=${props.city}}&aqi=no`)
+                .then(data => data.json())
+                .then(data => raw = data)
+    
+            console.log(raw);
+            setTemp(raw.current.feelslike_c);
+            setIcon(raw.current.condition.icon);
+            setLocation(`${raw.location.name}`);
+        } 
+        catch(error) {
+            console.log(error)
+            setTemp(`Unable to fetch resources`);
+            setLocation(`Unable to fetch resources`);
+        }
+        
     }
 
     getApi()
